58c5608bb8d470d416e925e24c8ff075f75c471d
funwarioisii <g231q026@s.iwate-pu.ac.jp>
2019-03-21 08:41:55 +0900

    構造化した

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,59 +13,70 @@ const database = admin.database();
  * @param toName 誰へ
  * @param price いくら
  */
-const writeHistory = ((fromName: string, toName: string, price: number) => {
+const writeHistory = (
+  (fromName: string, toName: string, price: number) => {
+    const now = Date.now()
+    database
+      .ref('history/' + now)
+      .set({
+        time: now,
+        fromName: fromName,
+        toName: toName,
+        price : price
+      })
+      .then(() => console.log("Write success"))
+      .catch((err) => console.error(err));
+});
 
-  const now = Date.now()
-  database
+/**
+ * @param fromName 誰から
+ * @param toName 誰へ
+ * @param price いくら
+ * @param description 内訳
+ */
+const writeHistoryWithDesc = (
+  (fromName: string, toName: string, price: number, description: string) => {
+    const now = Date.now()
+    database
     .ref('history/' + now)
     .set({
       time: now,
       fromName: fromName,
       toName: toName,
-      price : price
+      price : price,
+      description: description
     })
     .then(() => console.log("Write success"))
     .catch((err) => console.error(err));
-});
-
-const writeHistoryWithDesc = ((fromName: string, toName: string, price: number, description: string) =>{
-  const now = Date.now()
-  database
-  .ref('history/' + now)
-  .set({
-    time: now,
-    fromName: fromName,
-    toName: toName,
-    price : price,
-    description: description
-  })
-  .then(() => console.log("Write success"))
-  .catch((err) => console.error(err));
 })
+
+const readHistory = (): string =>  {
+  let responseBody: string = ""
+  database.ref('history')
+    .on('value', ((snapshot) => {
+      const result: object = snapshot.val()
+      for (const key in result) {
+        responseBody += `${key}\n`
+        const fromName: string = result[key].fromName
+        const toName: string = result[key].toName
+        const price: number = result[key].price
+        responseBody += `${fromName}から${toName}に${price}円渡してる\n`
+      }
+  }),
+  ((err) => {
+    console.error(err)
+    return `何らかのエラーが発生したかも $err`
+  }))
+  return responseBody
+}
 
 export const helloWorld = functions.https.onRequest((req, res) => {
   const messages = req.body.text.split(" ")
   if (messages[0] === "history") {
-    database.ref('history')
-      .on('value', ((snapshot) => {
-        const result = snapshot.val()
-        let responseBody = ""
-        for (const key in result) {
-          responseBody += `${key}\n`
-          const fromName = result[key].fromName
-          const toName = result[key].toName
-          const price = result[key].price
-          responseBody += `${fromName}から${toName}に${price}円渡してる\n`
-        }
-        // res.send(responseBody)
-
-        res.send({
-          response_type: "in_channel",
-          text: responseBody
-        })
-      }),
-      ((err) => console.error(err)))
-
+    res.send({
+      response_type: "in_channel",
+      text: readHistory()
+    })
   } else if (messages.length !== 3 && messages.length !== 4){
     res.send("augument error, [from: str] [to: str] [price: int]")
   } else if (messages.length === 3){
@@ -79,10 +90,10 @@ export const helloWorld = functions.https.onRequest((req, res) => {
       text: "recorded!"
     })
   } else if (messages.length === 4) {
-    const fromName = messages[0]
-    const toName = messages[1]
-    const price = messages[2]
-    const description = messages[3]
+    const fromName: string = messages[0]
+    const toName: string = messages[1]
+    const price: number = messages[2]
+    const description: string = messages[3]
 
     writeHistoryWithDesc(fromName, toName, price, description)
     res.send({
