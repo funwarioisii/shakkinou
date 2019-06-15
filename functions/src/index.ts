c4bcb7c8b94963daed838617c8ea8e2f8e74397b
funwarioisii <g231q026@s.iwate-pu.ac.jp>
2019-06-15 23:21:46 +0900

    emoji adaption

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,7 +15,7 @@ const database = admin.database();
  */
 const writeHistory = (
   (fromName: string, toName: string, price: number) => {
-    const now = Date.now()
+    const now = Date.now();
     database
       .ref('history/' + now)
       .set({
@@ -28,9 +28,16 @@ const writeHistory = (
       .catch((err) => console.error(err));
 });
 
+/**
+ * 
+ * @param fromName 誰から
+ * @param toName 誰へ
+ * @param price いくら
+ * @param description 理由
+ */
 const writeHistoryWithDesc = (
   (fromName: string, toName: string, price: number, description: string) => {
-    const now = Date.now()
+    const now = Date.now();
     database
     .ref('history/' + now)
     .set({
@@ -42,19 +49,28 @@ const writeHistoryWithDesc = (
     })
     .then(() => console.log("Write success"))
     .catch((err) => console.error(err));
-})
+});
+
+
+const nameConverter = (name: string) => {
+  if (!name.startsWith(":")) {
+    return `:${name}:`
+  } else {
+    return name
+  }
+}
 
 
 export const helloWorld = functions.https.onRequest((req, res) => {
-  const messages = req.body.text.split(" ")
+  const messages = req.body.text.split(" ");
   if (messages[0] === "history") {
     database.ref('history')
       .on('value', ((snapshot) => {
         const result = ((mess) => {
-          const _result = snapshot.val()
+          const _result = snapshot.val();
           if (mess.length >=2 && Number(mess[1])) {
-            const keys = Object.keys(_result).slice(Object.keys(_result).length - Number(messages[1]))
-            const _res = {}
+            const keys = Object.keys(_result).slice(Object.keys(_result).length - Number(messages[1]));
+            const _res = {};
             for (const key of keys) {
               _res[key] = _result[key]
             }
@@ -62,21 +78,21 @@ export const helloWorld = functions.https.onRequest((req, res) => {
           } else {
             return _result
           }
-        })(messages)
-        let responseBody = ""
+        })(messages);
+        let responseBody = "";
         for (const key in result) {
-          responseBody += `[${key}] `
-          const fromName = result[key].fromName
-          const toName = result[key].toName
-          const price = result[key].price
-          const description = result[key].description
+          responseBody += `[${key}] `;
+          const fromName = nameConverter(result[key].fromName);
+          const toName = nameConverter(result[key].toName);
+          const price = result[key].price;
+          const description = result[key].description;
           const breakdown = ((x) => {
             if (description === undefined) {
               return ""
             } else {
               return `（内訳：${description}）`
             }
-          })(description)
+          })(description);
           responseBody += `${fromName}から${toName}に${price}円渡してる．${breakdown}\n`
         }
 
@@ -96,17 +112,17 @@ export const helloWorld = functions.https.onRequest((req, res) => {
 
     database.ref('history')
       .on('value', ((snapshot) => {
-        const result = snapshot.val()
-        let accumulatePrice = {}
+        const result = snapshot.val();
+        let accumulatePrice = {};
 
-        Object.keys(result).forEach(key => accumulatePrice[result[key].fromName] = 0)
+        Object.keys(result).forEach(key => accumulatePrice[nameConverter(result[key].fromName)] = 0);
 
-        Object.keys(result).forEach(key => accumulatePrice[result[key].fromName] += Number(result[key].price))
+        Object.keys(result).forEach(key => accumulatePrice[nameConverter(result[key].fromName)] += Number(result[key].price));
 
-        let responseBody = ""
+        let responseBody = "";
         Object.keys(accumulatePrice).forEach((element) => {
           responseBody += `${element}は${accumulatePrice[element]}円貸してる \n`
-        })
+        });
 
         res.send({
           response_type: "in_channel",
@@ -116,24 +132,24 @@ export const helloWorld = functions.https.onRequest((req, res) => {
       ((err) => console.error(err)))
 
   } else if (messages.length !== 3 && messages.length !== 4){
-    res.send("augument error, [from: str] [to: str] [price: int]")
+    res.send("argument error, [from: str] [to: str] [price: int]")
   } else if (messages.length === 3){
-    const fromName = messages[0]
-    const toName = messages[1]
-    const price = messages[2]
+    const fromName = messages[0];
+    const toName = messages[1];
+    const price = messages[2];
 
-    writeHistory(fromName, toName, price)
+    writeHistory(fromName, toName, price);
     res.send({
       response_type: "in_channel",
       text: "recorded!"
     })
   } else if (messages.length === 4) {
-    const fromName: string = messages[0]
-    const toName: string = messages[1]
-    const price: number = messages[2]
-    const description: string = messages[3]
+    const fromName: string = messages[0];
+    const toName: string = messages[1];
+    const price: number = messages[2];
+    const description: string = messages[3];
 
-    writeHistoryWithDesc(fromName, toName, price, description)
+    writeHistoryWithDesc(fromName, toName, price, description);
     res.send({
       response_type: "in_channel",
       text: "recorded!"
